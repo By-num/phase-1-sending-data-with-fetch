b67cbe7224cdb2cf3df3fb769718f3ff5907ab99
By-num <bynumst3cy@gmail.com>
2025-06-27 19:28:11 +0300

    sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const newId = object.id;
+      const body = document.querySelector("body");
+      body.append(newId);
+    })
+    .catch(function (error) {
+      const body = document.querySelector("body");
+      body.append(error.message);
+    });
+}
